9d822644f99fcaea23a5bf2331ecf67f5149edb4
Tino Echaniz <tino@tinux.net>
2023-07-16 20:07:29 +0200

    Final Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.svg';
+
 
 function App() {
-  return (
-    <div className="App">
- 	Hola Tasks
-	  </div>
+    return (
+        <div className="tasks">
+	        <div className='freecodecamp-logo-contenedor'>
+                <img
+                    src={ freeCodeCampLogo }
+                    className='freecodecamp-logo'
+                />
+	        </div>
+            <div className='tasks-list-main'>
+                   <p>Hola Victoria</p>
+            </div>
+         </div>
   );
 }
 
